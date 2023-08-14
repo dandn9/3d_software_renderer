4c8c29f50fcea7683f24d7e164f6feaeb55b1e40
Dan <daniel.lauri22@gmail.com>
2023-08-14 18:22:18 +0200

    a

--- src/main.c
+++ src/main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 #include "display.h"
 #include "vector.h"
 
-
-////////////////////////////////////////////////////////////////////////
-// Declare an array of vectors / points
-////////////////////////////////////////////////////////////////////////
-
-
-#define N_POINTS ( 9 * 9 * 9)
-
+////////////////////////////////////////////////////////////////////////////////
+// Declare an array of vectors/points
+////////////////////////////////////////////////////////////////////////////////
+#define N_POINTS (9 * 9 * 9)
 vec3_t cube_points[N_POINTS]; // 9x9x9 cube
 vec2_t projected_points[N_POINTS];
 
@@ -20,48 +16,38 @@ float fov_factor = 128;
 
 bool is_running = false;
 
-
-void setup(void){
-
-  // Enable console output in SDL
-    SDL_SetHint(SDL_HINT_WINDOWS_ENABLE_MESSAGELOOP, "1");
-
-
-    /* Allocate the memory in bytes to hold the color buffer*/    
+void setup(void) {
+    // Allocate the required memory in bytes to hold the color buffer
     color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 
-
-    /* Creating a SDL Texture that is used to display the color_buffer */
+    // Creating a SDL texture that is used to display the color buffer
     color_buffer_texture = SDL_CreateTexture(
-        renderer, 
-        SDL_PIXELFORMAT_ARGB8888, 
-        SDL_TEXTUREACCESS_STREAMING, 
-        window_width, 
-        window_height);
-    
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
+
     int point_count = 0;
+
     // Start loading my array of vectors
-    // From -1 to 1 ( length 2 ) in 9x9x9 cube
-    for(float x = -1; x < 1; x += 0.25) {
-        for(float y = -1; y < 1; y += 0.25) {
-            for(float z = -1; z < 1; z += 0.25) {
-                vec3_t new_point = { .x = x, .y = y, .z = z};
+    // From -1 to 1 (in this 9x9x9 cube)
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y += 0.25) {
+            for (float z = -1; z <= 1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z };
                 cube_points[point_count++] = new_point;
             }
         }
     }
-
-
-    // SDL_LogSetOutputFunction()
-    printf("width: %d height %d, %s", window_width, window_height, cube_points);
-
-
 }
-void process_input(void){
+
+void process_input(void) {
     SDL_Event event;
     SDL_PollEvent(&event);
 
-    switch(event.type) {
+    switch (event.type) {
         case SDL_QUIT:
             is_running = false;
             break;
@@ -71,34 +57,43 @@ void process_input(void){
             break;
     }
 }
-////////////////////////////////////////////////////////////////////////
-// Function that receives a 3d vector and returns a projected 2d point
-////////////////////////////////////////////////////////////////////////
-vec2_t project(vec3_t point){
-    vec2_t projected_point = { 
-        .x = (fov_factor * point.x), 
+
+////////////////////////////////////////////////////////////////////////////////
+// Function that receives a 3D vector and returns a projected 2D point
+////////////////////////////////////////////////////////////////////////////////
+vec2_t project(vec3_t point) {
+    vec2_t projected_point = {
+        .x = (fov_factor * point.x),
         .y = (fov_factor * point.y)
     };
+    return projected_point;
 }
 
-void update(void){
-    for( int i = 0; i < N_POINTS; i++ ){
+void update(void) {
+    for (int i = 0; i < N_POINTS; i++) {
         vec3_t point = cube_points[i];
 
         // Project the current point
         vec2_t projected_point = project(point);
-        // Save the projected 2d point
+
+        // Save the projected 2D vector in the array of projected points
         projected_points[i] = projected_point;
     }
 }
 
-void render(void){
+void render(void) {
     draw_grid();
 
     // Loop all projected points and render them
-    for(int i = 0; i < N_POINTS; i++){
+    for (int i = 0; i < N_POINTS; i++) {
         vec2_t projected_point = projected_points[i];
-        draw_rect(projected_point.x + (window_width / 2), projected_point.y + (window_height / 2), 4, 5, 0xFFFFFF00);
+        draw_rect(
+            projected_point.x + (window_width / 2),
+            projected_point.y + (window_height / 2),
+            4,
+            4,
+            0xFFFFFF00
+        );
     }
 
     render_color_buffer();
@@ -108,12 +103,10 @@ void render(void){
     SDL_RenderPresent(renderer);
 }
 
-int main(int argc, char* args[]){
-    /* Create a SDL Window */
+int main(int argc, char *argv[]) {
     is_running = initialize_window();
 
     setup();
-    
 
     while (is_running) {
         process_input();
