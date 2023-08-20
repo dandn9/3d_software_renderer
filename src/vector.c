25fe2a9a437cf71aa2e3c9802689b5e27998c8c1
Dan <daniel.lauri22@gmail.com>
2023-08-20 19:46:02 +0200

    vec opetaions

--- src/vector.c
+++ src/vector.c
@@ -1,27 +1,43 @@
 #include "vector.h"
 #include <math.h>
 
-vec3_t vec3_rotate_x(vec3_t v, float angle){
+////////////////////////////////////////////////////////////////////////
+// Vec2D Functions
+////////////////////////////////////////////////////////////////////////
+float vec2_length(vec2_t v)
+{
+    return sqrt(v.x * v.x + v.y * v.y);
+}
+
+////////////////////////////////////////////////////////////////////////
+// Vec3D Functions
+////////////////////////////////////////////////////////////////////////
+float vec3_length(vec3_t v)
+{
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+vec3_t vec3_rotate_x(vec3_t v, float angle)
+{
     vec3_t rotated_vec = {
         .x = v.x,
         .y = v.y * cos(angle) - v.z * sin(angle),
-        .z = v.y * sin(angle) + v.z * cos(angle)
-    };
+        .z = v.y * sin(angle) + v.z * cos(angle)};
     return rotated_vec;
 }
-vec3_t vec3_rotate_y(vec3_t v, float angle){
+vec3_t vec3_rotate_y(vec3_t v, float angle)
+{
     vec3_t rotated_vec = {
         .x = v.x * cos(angle) - v.z * sin(angle),
         .y = v.y,
-        .z = v.x * sin(angle) + v.z * cos(angle)
-    };
+        .z = v.x * sin(angle) + v.z * cos(angle)};
     return rotated_vec;
 }
-vec3_t vec3_rotate_z(vec3_t v, float angle){
+vec3_t vec3_rotate_z(vec3_t v, float angle)
+{
     vec3_t rotated_vec = {
         .x = v.x * cos(angle) - v.y * sin(angle),
         .y = v.x * sin(angle) + v.y * cos(angle),
-        .z = v.z
-    };
+        .z = v.z};
     return rotated_vec;
 }
--- src/vector.h
+++ src/vector.h
@@ -1,16 +1,32 @@
 #pragma once
 
-typedef struct {
+typedef struct
+{
     float x;
     float y;
 } vec2_t;
 
-typedef struct {
+typedef struct
+{
     float x;
     float y;
     float z;
 } vec3_t;
 
+////////////////////////////////////////////////////////////////////////
+// Vec2D
+////////////////////////////////////////////////////////////////////////
+
+float vec2_length(vec2_t v);
+vec2_t vec2_add(vec2_t a, vec2_t, b);
+vec2_t vec2_sub(vec2_t a, vec2_t, b);
+
+////////////////////////////////////////////////////////////////////////
+// Vec3D
+////////////////////////////////////////////////////////////////////////
+
+vec3_t vec3_add(vec3_t a, vec3_t, b);
+vec3_t vec3_sub(vec3_t a, vec3_t, b);
 
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
